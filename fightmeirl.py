589d89ed55cac1a01508aca9c533269b9d7c0048
Brandon Navarro <brandon.navarro@kearsarge.org>
2015-01-14 13:27:08 -0500

    Now with menu

--- fightmeirl.py
+++ fightmeirl.py
@@ -9,84 +9,95 @@ clock = pygame.time.Clock()
 width = 896
 height = 700
 size = width, height
-
 screen = pygame.display.set_mode(size)
 
 bgColor = r,g,b = 0, 0, 0
-
-bgI = pygame.image.load("Map.png")
-bgR = bgI.get_rect()
-
-screen = pygame.display.set_mode(size)
+bgImage = pygame.image.load("Menu.png").convert()
+bgRect = bgImage.get_rect()
 
 player1 = Player([800,598]) 
 player2 = Player([100,598]) 
 
-
-#player1 = Player("p1_walk03.png" ,[0,0],[800,598]) 
-#player2 = Player("p1_walk03.png",[0,0],[100,598]) 
-
 bullets = []
 
+run = False
+
 while True:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT: sys.exit()
-		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_w:
-				player1.go("up")
-			if event.key == pygame.K_d:
-				player1.go("right")
-			if event.key == pygame.K_a:
-				player1.go("left")
-			if event.key == pygame.K_SPACE:
-				bullets += player1.shoot("fire")
-			if event.key == pygame.K_UP:
-				player2.go("up")
-			if event.key == pygame.K_RIGHT:
-				player2.go("right")
-			if event.key == pygame.K_LEFT:
-				player2.go("left")
-			if event.key == pygame.K_l:
-				bullets += player2.shoot("fire")
-		if event.type == pygame.KEYUP:
-			if event.key == pygame.K_w:
-				player1.go("stop up")
-			if event.key == pygame.K_d:
-				player1.go("stop right")
-			if event.key == pygame.K_a:
-				player1.go("stop left")
-			if event.key == pygame.K_SPACE:
-				player1.shoot("stop")
-			if event.key == pygame.K_UP:
-				player2.go("stop up")
-			if event.key == pygame.K_RIGHT:
-				player2.go("stop right")
-			if event.key == pygame.K_LEFT:
-				player2.go("stop left")
-			if event.key == pygame.K_l:
-				player2.shoot("stop")
+    while not run:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT: sys.exit()
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_RETURN:
+					run = True
+        					
+		bgColor = r,g,b
+		screen.fill(bgColor)
+		screen.blit(bgImage, bgRect)
+		pygame.display.flip()
+		clock.tick(60)
+    
+    bgImage = pygame.image.load("Map.png").convert()
+    bgRect = bgImage.get_rect()
+    
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    player1.go("up")
+                if event.key == pygame.K_d:
+                    player1.go("right")
+                if event.key == pygame.K_a:
+                    player1.go("left")
+                if event.key == pygame.K_SPACE:
+                    bullets += player1.shoot("fire")
+                if event.key == pygame.K_UP:
+                    player2.go("up")
+                if event.key == pygame.K_RIGHT:
+                    player2.go("right")
+                if event.key == pygame.K_LEFT:
+                    player2.go("left")
+                if event.key == pygame.K_l:
+                    bullets += player2.shoot("fire")
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_w:
+                    player1.go("stop up")
+                if event.key == pygame.K_d:
+                    player1.go("stop right")
+                if event.key == pygame.K_a:
+                    player1.go("stop left")
+                if event.key == pygame.K_SPACE:
+                    player1.shoot("stop")
+                if event.key == pygame.K_UP:
+                    player2.go("stop up")
+                if event.key == pygame.K_RIGHT:
+                    player2.go("stop right")
+                if event.key == pygame.K_LEFT:
+                    player2.go("stop left")
+                if event.key == pygame.K_l:
+                    player2.shoot("stop")
+                    
+                    
+        player1.update(width, height)	
                 
-				
-	player1.update(width, height)	
-			
-	player2.update(width, height)
-	
-	for bullet in bullets:
-		bullet.update(width, height)
-		   
-	for bullet in bullets:
-		if not bullet.living:
-			bullets.remove(bullet)
+        player2.update(width, height)
         
-        
+        for bullet in bullets:
+            bullet.update(width, height)
+               
+        for bullet in bullets:
+            if not bullet.living:
+                bullets.remove(bullet)
+            
+            
 
 
-	bgColor = r,g,b
-	screen.fill(bgColor)
-	screen.blit(bgI, bgR)
-	for bullet in bullets:
-		screen.blit(bullet.image, bullet.rect)
-	screen.blit(player1.image, player1.rect)
-	screen.blit(player2.image, player2.rect)
-	pygame.display.flip()
-	clock.tick(60)
+        bgColor = r,g,b
+        screen.fill(bgColor)
+        screen.blit(bgImage, bgRect)
+        for bullet in bullets:
+            screen.blit(bullet.image, bullet.rect)
+        screen.blit(player1.image, player1.rect)
+        screen.blit(player2.image, player2.rect)
+        pygame.display.flip()
+        clock.tick(60)
