726c31dfc49c57dfc6bef5a008ce17f927d11f78
Brandon Navarro <brandon.navarro@kearsarge.org>
2015-02-04 12:27:23 -0500

    i puncheded ur fasseeded

--- melee.py
+++ melee.py
@@ -1,1 +1,11 @@
+import pygame
+from Ball import Ball
+
+class Melee(Ball):
+    def __init__(self, pos, bspeed, direction, heading2 = None):
+		Ball.__init__(self, "Trentsface.png", [0,0], pos)
+		if direction == "right" or direction == "up" and "right":
+				self.speedx = self.maxSpeed
+		if direction == "left" or direction == "up" and "left": 
+				self.speedx = -self.maxSpeed
 
